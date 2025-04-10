ad6350f9f3998a5a9e7cb006b9c118a33d38ed7d
Daniel Villarreal <156235653+DanielVg676@users.noreply.github.com>
2025-04-09 21:01:46 -0600

    Update paymentController.js

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -8,7 +8,7 @@ dotenv.config();
 const PAYPAL_API = "https://api-m.sandbox.paypal.com";
 const PAYPAL_API_CLIENT = process.env.PAYPAL_API_CLIENT;
 const PAYPAL_API_SECRET = process.env.PAYPAL_API_SECRET;
-const HOST = "http://localhost:3005";
+const HOST = "http://payment-production-bec3.up.railway.app";
 
 if (!PAYPAL_API_CLIENT || !PAYPAL_API_SECRET) {
   console.error("PAYPAL_API_CLIENT and PAYPAL_API_SECRET must be set in environment variables.");
